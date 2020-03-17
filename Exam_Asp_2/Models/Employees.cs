2125fb06e474833a4a59231d91bec985eeaedfcd
namlephuong95 <namlephuong95@gmail.com>
2020-03-17 20:37:41 +0700

    Final_Asp

---
+++ Exam_Asp_2/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Exam_Asp_2.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
